1a689fe2a9c9550898e0e14e2209221fa44b1bfc
5kbpers <tangminghua@pingcap.com>
2019-08-29 11:35:26 +0800

    remove unused scripts
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- show_test_table.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-echo ">>>>>>>>>>>>>>>> source table"
-mysql -u root -h 172.16.5.36 -P 42002 -p -e "use sbtest; select * from sbtest1;"
-
-echo ">>>>>>>>>>>>>>>> destination table"
-mysql -u root -h 172.16.5.36 -P 42002 -p -e "use sbtest1; select * from sbtest1;"
