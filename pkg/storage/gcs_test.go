d6fda29f897ea87ac361bc5bdd4d5e573fffef0a
Feng Liyuan <darktemplar.f@gmail.com>
2019-12-12 15:44:09 +0800

    update test

--- pkg/storage/gcs_test.go
+++ pkg/storage/gcs_test.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"io/ioutil"
 
 	"github.com/fsouza/fake-gcs-server/fakestorage"
 	. "github.com/pingcap/check"
@@ -14,9 +15,7 @@ var _ = Suite(&testSuite{})
 
 func (r *testSuite) TestGCS(c *C) {
 	opts := fakestorage.Options{
-		StorageRoot: "",
-		Host:        "0.0.0.0",
-		Port:        0,
+		NoListener: true,
 	}
 	server, err := fakestorage.NewServerWithOptions(opts)
 	c.Assert(err, IsNil)
@@ -34,13 +33,23 @@ func (r *testSuite) TestGCS(c *C) {
 	ctx := context.Background()
 	err = stg.Write(ctx, "key", []byte("data"))
 	c.Assert(err, IsNil)
-	d, err := stg.Read(ctx, "key")
+
+	rc, err := server.Client().Bucket(bucketName).Object("a/b/key").NewReader(ctx)
+	c.Assert(err, IsNil)
+	d, err := ioutil.ReadAll(rc)
+	rc.Close()
 	c.Assert(err, IsNil)
 	c.Assert(d, DeepEquals, []byte("data"))
+
+	d, err = stg.Read(ctx, "key")
+	c.Assert(err, IsNil)
+	c.Assert(d, DeepEquals, []byte("data"))
+
 	exist, err := stg.FileExists(ctx, "key")
 	c.Assert(err, IsNil)
 	c.Assert(exist, IsTrue)
-	exist, err = stg.FileExists(ctx, "not_exist")
+
+	exist, err = stg.FileExists(ctx, "key_not_exist")
 	c.Assert(err, IsNil)
 	c.Assert(exist, IsFalse)
 }
