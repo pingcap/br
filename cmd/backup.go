ca845834a901039f9a9fa1dd0f318a78ddc22d64
Neil Shen <overvenus@gmail.com>
2019-12-13 17:18:27 +0800

    *: support backup database (#111)
    * *: support backup database
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    * tests: adjust tests
    
    - fastchecksum is enabled by default
    - use backup db for br_db
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    * fix get region count
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    * remove flag fastchecksum
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- cmd/backup.go
+++ cmd/backup.go
@@ -8,17 +8,151 @@ import (
 	"github.com/pingcap/tidb/ddl"
 	"github.com/pingcap/tidb/session"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	"github.com/pingcap/br/pkg/backup"
 	"github.com/pingcap/br/pkg/storage"
 	"github.com/pingcap/br/pkg/utils"
 )
 
+const (
+	flagBackupTimeago     = "timeago"
+	flagBackupRateLimit   = "ratelimit"
+	flagBackupConcurrency = "concurrency"
+	flagBackupChecksum    = "checksum"
+	flagBackupDB          = "db"
+	flagBackupTable       = "table"
+)
+
+func defineBackupFlags(flagSet *pflag.FlagSet) {
+	flagSet.StringP(
+		flagBackupTimeago, "", "",
+		"The history version of the backup task, e.g. 1m, 1h. Do not exceed GCSafePoint")
+	flagSet.Uint64P(
+		flagBackupRateLimit, "", 0, "The rate limit of the backup task, MB/s per node")
+	flagSet.Uint32P(
+		flagBackupConcurrency, "", 4, "The size of thread pool on each node that execute the backup task")
+	flagSet.BoolP(flagBackupChecksum, "", true,
+		"Run checksum after backup")
+}
+
+func runBackup(flagSet *pflag.FlagSet, cmdName, db, table string) error {
+	ctx, cancel := context.WithCancel(defaultContext)
+	defer cancel()
+
+	mgr, err := GetDefaultMgr()
+	if err != nil {
+		return err
+	}
+	defer mgr.Close()
+
+	timeago, err := flagSet.GetString(flagBackupTimeago)
+	if err != nil {
+		return err
+	}
+
+	ratelimit, err := flagSet.GetUint64(flagBackupRateLimit)
+	if err != nil {
+		return err
+	}
+
+	concurrency, err := flagSet.GetUint32(flagBackupConcurrency)
+	if err != nil {
+		return err
+	}
+	if concurrency == 0 {
+		err = errors.New("at least one thread required")
+		return err
+	}
+
+	checksum, err := flagSet.GetBool(flagBackupChecksum)
+	if err != nil {
+		return err
+	}
+
+	u, err := storage.ParseBackendFromFlags(flagSet, FlagStorage)
+	if err != nil {
+		return err
+	}
+
+	client, err := backup.NewBackupClient(ctx, mgr)
+	if err != nil {
+		return nil
+	}
+
+	err = client.SetStorage(ctx, u)
+	if err != nil {
+		return err
+	}
+
+	backupTS, err := client.GetTS(ctx, timeago)
+	if err != nil {
+		return err
+	}
+
+	ranges, backupSchemas, err := backup.BuildBackupRangeAndSchema(
+		mgr.GetDomain(), mgr.GetTiKV(), backupTS, db, table)
+	if err != nil {
+		return err
+	}
+
+	// The number of regions need to backup
+	approximateRegions := 0
+	for _, r := range ranges {
+		var regionCount int
+		regionCount, err = mgr.GetRegionCount(ctx, r.StartKey, r.EndKey)
+		if err != nil {
+			return err
+		}
+		approximateRegions += regionCount
+	}
+
+	// Backup
+	// Redirect to log if there is no log file to avoid unreadable output.
+	updateCh := utils.StartProgress(
+		ctx, cmdName, int64(approximateRegions), !HasLogFile())
+	err = client.BackupRanges(
+		ctx, ranges, backupTS, ratelimit, concurrency, updateCh)
+	if err != nil {
+		return err
+	}
+	// Backup has finished
+	close(updateCh)
+
+	// Checksum
+	backupSchemasConcurrency := backup.DefaultSchemaConcurrency
+	if backupSchemas.Len() < backupSchemasConcurrency {
+		backupSchemasConcurrency = backupSchemas.Len()
+	}
+	updateCh = utils.StartProgress(
+		ctx, "Checksum", int64(backupSchemas.Len()), !HasLogFile())
+	backupSchemas.SetSkipChecksum(!checksum)
+	backupSchemas.Start(
+		ctx, mgr.GetTiKV(), backupTS, uint(backupSchemasConcurrency), updateCh)
+
+	err = client.CompleteMeta(backupSchemas)
+	if err != nil {
+		return err
+	}
+
+	valid, err := client.FastChecksum()
+	if err != nil {
+		return err
+	}
+	if !valid {
+		log.Error("backup FastChecksum failed!")
+	}
+	// Checksum has finished
+	close(updateCh)
+
+	return client.SaveBackupMeta(ctx)
+}
+
 // NewBackupCommand return a full backup subcommand.
 func NewBackupCommand() *cobra.Command {
 	command := &cobra.Command{
 		Use:   "backup",
-		Short: "backup a TiKV cluster",
+		Short: "backup a TiDB cluster",
 		PersistentPreRunE: func(c *cobra.Command, args []string) error {
 			if err := Init(c); err != nil {
 				return err
@@ -35,24 +169,11 @@ func NewBackupCommand() *cobra.Command {
 	}
 	command.AddCommand(
 		newFullBackupCommand(),
+		newDbBackupCommand(),
 		newTableBackupCommand(),
 	)
 
-	command.PersistentFlags().StringP(
-		"timeago", "", "",
-		"The history version of the backup task, e.g. 1m, 1h. Do not exceed GCSafePoint")
-
-	command.PersistentFlags().Uint64P(
-		"ratelimit", "", 0, "The rate limit of the backup task, MB/s per node")
-	command.PersistentFlags().Uint32P(
-		"concurrency", "", 4, "The size of thread pool on each node that execute the backup task")
-	command.PersistentFlags().BoolP("checksum", "", true,
-		"Run checksum after backup")
-
-	command.PersistentFlags().BoolP("fastchecksum", "", false,
-		"fast checksum backup sst file by calculate all sst file")
-
-	_ = command.PersistentFlags().MarkHidden("fastchecksum")
+	defineBackupFlags(command.PersistentFlags())
 	return command
 }
 
@@ -60,120 +181,34 @@ func NewBackupCommand() *cobra.Command {
 func newFullBackupCommand() *cobra.Command {
 	command := &cobra.Command{
 		Use:   "full",
-		Short: "backup the whole TiKV cluster",
+		Short: "backup all database",
 		RunE: func(command *cobra.Command, _ []string) error {
-			ctx, cancel := context.WithCancel(defaultContext)
-			defer cancel()
-
-			mgr, err := GetDefaultMgr()
-			if err != nil {
-				return err
-			}
-			defer mgr.Close()
-
-			client, err := backup.NewBackupClient(ctx, mgr)
-			if err != nil {
-				return nil
-			}
-			u, err := storage.ParseBackendFromFlags(command.Flags(), FlagStorage)
-			if err != nil {
-				return err
-			}
-
-			err = client.SetStorage(ctx, u)
-			if err != nil {
-				return err
-			}
-
-			timeAgo, err := command.Flags().GetString("timeago")
-			if err != nil {
-				return err
-			}
-
-			backupTS, err := client.GetTS(ctx, timeAgo)
-			if err != nil {
-				return err
-			}
-
-			rate, err := command.Flags().GetUint64("ratelimit")
-			if err != nil {
-				return err
-			}
-
-			concurrency, err := command.Flags().GetUint32("concurrency")
-			if err != nil {
-				return err
-			}
-			if concurrency == 0 {
-				return errors.New("at least one thread required")
-			}
-
-			checksum, err := command.Flags().GetBool("checksum")
-			if err != nil {
-				return err
-			}
-			fastChecksum, err := command.Flags().GetBool("fastchecksum")
-			if err != nil {
-				return err
-			}
-
-			ranges, backupSchemas, err := backup.BuildBackupRangeAndSchema(
-				mgr.GetDomain(), mgr.GetTiKV(), backupTS, "", "")
-			if err != nil {
-				return err
-			}
-
-			// the count of regions need to backup
-			approximateRegions, err := client.GetRangeRegionCount([]byte{}, []byte{})
-			if err != nil {
-				return err
-			}
-
-			// Backup
-			// Redirect to log if there is no log file to avoid unreadable output.
-			updateCh := utils.StartProgress(
-				ctx, "Full Backup", int64(approximateRegions), !HasLogFile())
-			err = client.BackupRanges(
-				ctx, ranges, backupTS, rate, concurrency, updateCh)
-			if err != nil {
-				return err
-			}
-			// Backup has finished
-			close(updateCh)
-
-			// Checksum
-			backupSchemasConcurrency := backup.DefaultSchemaConcurrency
-			if backupSchemas.Len() < backupSchemasConcurrency {
-				backupSchemasConcurrency = backupSchemas.Len()
-			}
-			cksctx, ckscancel := context.WithCancel(defaultContext)
-			defer ckscancel()
-			updateCh = utils.StartProgress(
-				cksctx, "Checksum", int64(backupSchemas.Len()), !HasLogFile())
-			backupSchemas.SetSkipChecksum(!checksum)
-			backupSchemas.Start(
-				cksctx, mgr.GetTiKV(), backupTS, uint(backupSchemasConcurrency), updateCh)
-
-			err = client.CompleteMeta(backupSchemas)
-			if err != nil {
-				return err
-			}
-
-			if fastChecksum {
-				valid, err := client.FastChecksum()
-				if err != nil {
-					return err
-				}
-				if !valid {
-					log.Error("backup FastChecksum not passed!")
-				}
-			}
-			// Checksum has finished
-			close(updateCh)
-
-			return client.SaveBackupMeta(ctx)
+			// empty db/table means full backup.
+			return runBackup(command.Flags(), "Full backup", "", "")
 		},
 	}
+	return command
+}
+
+// newDbBackupCommand return a db backup subcommand.
+func newDbBackupCommand() *cobra.Command {
+	command := &cobra.Command{
+		Use:   "db",
+		Short: "backup a database",
+		RunE: func(command *cobra.Command, _ []string) error {
+			db, err := command.Flags().GetString(flagBackupDB)
+			if err != nil {
+				return err
+			}
+			if len(db) == 0 {
+				return errors.Errorf("empty database name is not allowed")
+			}
+			return runBackup(command.Flags(), "Database backup", db, "")
+		},
+	}
+	command.Flags().StringP(flagBackupDB, "", "", "backup a table in the specific db")
+	_ = command.MarkFlagRequired(flagBackupDB)
+
 	return command
 }
 
@@ -183,37 +218,14 @@ func newTableBackupCommand() *cobra.Command {
 		Use:   "table",
 		Short: "backup a table",
 		RunE: func(command *cobra.Command, _ []string) error {
-			ctx, cancel := context.WithCancel(defaultContext)
-			defer cancel()
-
-			mgr, err := GetDefaultMgr()
-			if err != nil {
-				return err
-			}
-			defer mgr.Close()
-
-			client, err := backup.NewBackupClient(ctx, mgr)
-			if err != nil {
-				return err
-			}
-			u, err := storage.ParseBackendFromFlags(command.Flags(), FlagStorage)
-			if err != nil {
-				return err
-			}
-
-			err = client.SetStorage(ctx, u)
-			if err != nil {
-				return err
-			}
-
-			db, err := command.Flags().GetString("db")
+			db, err := command.Flags().GetString(flagBackupDB)
 			if err != nil {
 				return err
 			}
 			if len(db) == 0 {
 				return errors.Errorf("empty database name is not allowed")
 			}
-			table, err := command.Flags().GetString("table")
+			table, err := command.Flags().GetString(flagBackupTable)
 			if err != nil {
 				return err
 			}
@@ -221,101 +233,12 @@ func newTableBackupCommand() *cobra.Command {
 				return errors.Errorf("empty table name is not allowed")
 			}
 
-			timeAgo, err := command.Flags().GetString("timeago")
-			if err != nil {
-				return err
-			}
-
-			backupTS, err := client.GetTS(ctx, timeAgo)
-			if err != nil {
-				return err
-			}
-
-			rate, err := command.Flags().GetUint64("ratelimit")
-			if err != nil {
-				return err
-			}
-			concurrency, err := command.Flags().GetUint32("concurrency")
-			if err != nil {
-				return err
-			}
-			if concurrency == 0 {
-				return errors.New("at least one thread required")
-			}
-			checksum, err := command.Flags().GetBool("checksum")
-			if err != nil {
-				return err
-			}
-			fastChecksum, err := command.Flags().GetBool("fastchecksum")
-			if err != nil {
-				return err
-			}
-
-			ranges, backupSchemas, err := backup.BuildBackupRangeAndSchema(
-				mgr.GetDomain(), mgr.GetTiKV(), backupTS, db, table)
-			if err != nil {
-				return err
-			}
-
-			// The number of regions need to backup
-			approximateRegions := 0
-			for _, r := range ranges {
-				var regionCount int
-				regionCount, err = client.GetRangeRegionCount(r.StartKey, r.EndKey)
-				if err != nil {
-					return err
-				}
-				approximateRegions += regionCount
-			}
-
-			// Backup
-			// Redirect to log if there is no log file to avoid unreadable output.
-			updateCh := utils.StartProgress(
-				ctx, "Table Backup", int64(approximateRegions), !HasLogFile())
-			err = client.BackupRanges(
-				ctx, ranges, backupTS, rate, concurrency, updateCh)
-			if err != nil {
-				return err
-			}
-			// Backup has finished
-			close(updateCh)
-
-			// Checksum
-			cksctx, ckscancel := context.WithCancel(defaultContext)
-			defer ckscancel()
-			updateCh = utils.StartProgress(
-				cksctx, "Checksum", int64(backupSchemas.Len()), !HasLogFile())
-			backupSchemas.SetSkipChecksum(!checksum)
-			backupSchemas.Start(
-				cksctx, mgr.GetTiKV(), backupTS, 1, updateCh)
-
-			err = client.CompleteMeta(backupSchemas)
-			if err != nil {
-				return err
-			}
-
-			if fastChecksum {
-				valid, err := client.FastChecksum()
-				if err != nil {
-					return err
-				}
-				if !valid {
-					log.Error("backup FastChecksum not passed!")
-				}
-			}
-			// Checksum has finished
-			close(updateCh)
-
-			return client.SaveBackupMeta(ctx)
+			return runBackup(command.Flags(), "Table backup", db, table)
 		},
 	}
-	command.Flags().StringP("db", "", "", "backup a table in the specific db")
-	command.Flags().StringP("table", "t", "", "backup the specific table")
-	if err := command.MarkFlagRequired("db"); err != nil {
-		panic(err)
-	}
-	if err := command.MarkFlagRequired("table"); err != nil {
-		panic(err)
-	}
+	command.Flags().StringP(flagBackupDB, "", "", "backup a table in the specific db")
+	command.Flags().StringP(flagBackupTable, "t", "", "backup the specific table")
+	_ = command.MarkFlagRequired(flagBackupDB)
+	_ = command.MarkFlagRequired(flagBackupTable)
 	return command
 }
--- tests/br_shuffle_leader/run.sh
+++ tests/br_shuffle_leader/run.sh
@@ -29,7 +29,7 @@ echo "-u $PD_ADDR -d sched add shuffle-leader-scheduler" | pd-ctl
 
 # backup with shuffle leader
 echo "backup start..."
-run_br --pd $PD_ADDR backup table -s "local://$TEST_DIR/$DB" --db $DB -t $TABLE --ratelimit 1 --concurrency 4 --fastchecksum true
+run_br --pd $PD_ADDR backup table -s "local://$TEST_DIR/$DB" --db $DB -t $TABLE --ratelimit 1 --concurrency 4
 
 run_sql "DROP TABLE $DB.$TABLE;"
 
