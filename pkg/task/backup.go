56e151ad8b672272772cd2e37c6c26b148d9942e
3pointer <luancheng@pingcap.com>
2020-06-17 20:04:02 +0800

    br: add lock file (#358)

--- pkg/task/backup.go
+++ pkg/task/backup.go
@@ -125,6 +125,10 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 	if err = client.SetStorage(ctx, u, cfg.SendCreds); err != nil {
 		return err
 	}
+	err = client.SetLockFile(ctx)
+	if err != nil {
+		return err
+	}
 	client.SetGCTTL(cfg.GCTTL)
 
 	backupTS, err := client.GetTS(ctx, cfg.TimeAgo, cfg.BackupTS)
