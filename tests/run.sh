80043c14a185503775e766cc67590176e722513e
yujuncen <maruruku@stu.csust.edu.cn>
2020-06-18 16:20:17 +0800

    tests: add retry on start services

--- tests/run.sh
+++ tests/run.sh
@@ -21,7 +21,15 @@ mkdir -p "$TEST_DIR"
 rm -f "$TEST_DIR"/*.log
 
 trap stop_services EXIT
-start_services
+retry=0
+while ! start_services; do
+    if [ $retry -gt 3 ]; then
+        echo "failed to start service after $retry times, aborting..."
+        exit 1
+    fi
+    echo "service seems cannot be started up, retrying after $(( $retry * 30 )) seconds..."
+    sleep $(( $retry * 30 ))
+done
 
 if [ "${1-}" = '--debug' ]; then
     echo 'You may now debug from another terminal. Press [ENTER] to continue.'
