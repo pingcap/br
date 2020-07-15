de2d6e6673ed04a18d918cbedeef5b7b0f11dd6f
山岚 <36239017+YuJuncen@users.noreply.github.com>
2020-07-15 15:55:32 +0800

    glue: set no register itself to dashboard (#421)
    * glue: set no register itself to dashboard
    
    * glue: set no register when initlizating gluetidb

--- pkg/restore/db_test.go
+++ pkg/restore/db_test.go
@@ -74,7 +74,7 @@ func (s *testRestoreSchemaSuite) TestRestoreAutoIncID(c *C) {
 	c.Assert(autoIncID, Equals, uint64(globalAutoID))
 	// Alter AutoIncID to the next AutoIncID + 100
 	table.Info.AutoIncID = globalAutoID + 100
-	db, err := restore.NewDB(gluetidb.Glue{}, s.mock.Storage)
+	db, err := restore.NewDB(gluetidb.New(), s.mock.Storage)
 	c.Assert(err, IsNil, Commentf("Error create DB"))
 	tk.MustExec("drop database if exists test;")
 	// Test empty collate value
