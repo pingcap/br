b6ffa28a759794dcf5c5baa4a904bb196048806e
glorv <glorvs@163.com>
2020-08-13 14:42:18 +0800

    storage: extend the `ExternalStorage` interface and implements (#446)
    * update storage
    
    * update
    
    * fix
    
    * export s3 flags
    
    * fix local path
    
    * fix s3 reader
    
    * fix range offset
    
    * fix the req range errors
    
    * fix comments
    
    * fix review comments
    
    * fix some comments
    
    * update
    
    * fix
    
    * update
    
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -4,10 +4,18 @@ package storage
 
 import (
 	"context"
+	"io"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/kvproto/pkg/backup"
 )
+
+// ReadSeekCloser is the interface that groups the basic Read, Seek and Close methods.
+type ReadSeekCloser interface {
+	io.Reader
+	io.Seeker
+	io.Closer
+}
 
 // ExternalStorage represents a kind of file system storage.
 type ExternalStorage interface {
@@ -17,13 +25,22 @@ type ExternalStorage interface {
 	Read(ctx context.Context, name string) ([]byte, error)
 	// FileExists return true if file exists
 	FileExists(ctx context.Context, name string) (bool, error)
+	// Open a Reader by file name.
+	Open(ctx context.Context, name string) (ReadSeekCloser, error)
+	// WalkDir traverse all the files in a dir.
+	//
+	// fn is the function called for each regular file visited by WalkDir.
+	// The argument `path` is the file path that can be used in `Open`
+	// function; the argument `size` is the size in byte of the file determined
+	// by path.
+	WalkDir(ctx context.Context, fn func(path string, size int64) error) error
 }
 
 // Create creates ExternalStorage.
 func Create(ctx context.Context, backend *backup.StorageBackend, sendCreds bool) (ExternalStorage, error) {
 	switch backend := backend.Backend.(type) {
 	case *backup.StorageBackend_Local:
-		return newLocalStorage(backend.Local.Path)
+		return NewLocalStorage(backend.Local.Path)
 	case *backup.StorageBackend_S3:
 		if backend.S3 == nil {
 			return nil, errors.New("s3 config not found")
