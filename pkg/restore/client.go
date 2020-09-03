89879c7bf40d70fe9383a9b9d4f9205a7fdb8496
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-09-03 15:17:51 +0800

    summary: add real time cost to log collector (#480) (#486)
    Signed-off-by: Hillium <maruruku@stu.csust.edu.cn>

--- pkg/restore/client.go
+++ pkg/restore/client.go
@@ -818,6 +818,7 @@ func (rc *Client) GoValidateChecksum(
 			}
 			elapsed := time.Since(start)
 			summary.CollectDuration("restore checksum", elapsed)
+			summary.CollectSuccessUnit("table checksum", 1, elapsed)
 			outCh <- struct{}{}
 			close(outCh)
 		}()
