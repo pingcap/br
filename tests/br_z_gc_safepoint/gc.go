3e80ace85c49e9f6b39a9aa3757deb63910c58c8
Zheng Xiangsheng <hundundm@gmail.com>
2020-09-03 16:26:31 +0800

    update tidb-tools / pd
    Signed-off-by: Zheng Xiangsheng <hundundm@gmail.com>

--- tests/br_z_gc_safepoint/gc.go
+++ tests/br_z_gc_safepoint/gc.go
@@ -21,8 +21,8 @@ import (
 	"time"
 
 	"github.com/pingcap/log"
-	pd "github.com/pingcap/pd/v4/client"
 	"github.com/pingcap/tidb/store/tikv/oracle"
+	pd "github.com/tikv/pd/client"
 	"go.uber.org/zap"
 )
 
