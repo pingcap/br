1d86f29efa639167634234be231cc8f38ee08b97
Jon Tirsen <jontirsen@squareup.com>
2020-09-23 11:27:15 +0200

    BufferWriter for testing

--- pkg/storage/writer.go
+++ pkg/storage/writer.go
@@ -69,3 +69,39 @@ func newUploaderWriter(uploader Uploader, chunkSize int) *uploaderWriter {
 		uploader: uploader,
 		buf:      bytes.NewBuffer(make([]byte, 0, chunkSize))}
 }
+
+// BufferWriter is a Writer implementation on top of bytes.Buffer that is useful for testing
+type BufferWriter struct {
+	buf *bytes.Buffer
+}
+
+// Write delegates to bytes.Buffer
+func (u *BufferWriter) Write(ctx context.Context, p []byte) (int, error) {
+	return u.buf.Write(p)
+}
+
+// Close delegates to bytes.Buffer
+func (u *BufferWriter) Close(ctx context.Context) error {
+	// noop
+	return nil
+}
+
+// Bytes delegates to bytes.Buffer
+func (u *BufferWriter) Bytes() []byte {
+	return u.buf.Bytes()
+}
+
+// String delegates to bytes.Buffer
+func (u *BufferWriter) String() string {
+	return u.buf.String()
+}
+
+// Reset delegates to bytes.Buffer
+func (u *BufferWriter) Reset() {
+	u.buf.Reset()
+}
+
+// NewBufferWriter creates a Writer that simply writes to a buffer (useful for testing)
+func NewBufferWriter() *BufferWriter {
+	return &BufferWriter{buf: &bytes.Buffer{}}
+}
