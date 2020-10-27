9e789b8fb5505ad5d4a128f722d764d6b9f15698
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-10-28 00:17:58 +0800

    fix dot

--- pkg/storage/writer.go
+++ pkg/storage/writer.go
@@ -8,15 +8,15 @@ import (
 	"io"
 )
 
-// CompressType represents the type of compression
+// CompressType represents the type of compression.
 type CompressType uint8
 
 const (
-	// NoCompression won't compress given bytes
+	// NoCompression won't compress given bytes.
 	NoCompression CompressType = iota
-	// Gzip will compress given bytes in gzip format
+	// Gzip will compress given bytes in gzip format.
 	Gzip
-	// Zlib will compress give bytes in zlib format
+	// Zlib will compress give bytes in zlib format.
 	Zlib
 )
 
