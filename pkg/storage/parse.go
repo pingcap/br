b3fa178a9d740c0c8310e757b94640a04e471237
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-11-24 20:47:25 +0800

    handle special character in URL

--- pkg/storage/parse.go
+++ pkg/storage/parse.go
@@ -29,6 +29,7 @@ func ParseBackend(rawURL string, options *BackendOptions) (*backup.StorageBacken
 		return nil, errors.Annotate(berrors.ErrStorageInvalidConfig, "empty store is not allowed")
 	}
 
+	rawURL = strings.ReplaceAll(rawURL, "+", "%2B")
 	u, err := url.Parse(rawURL)
 	if err != nil {
 		return nil, errors.Trace(err)
