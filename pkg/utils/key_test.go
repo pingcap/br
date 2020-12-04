e146662edd56221a02dca2105b4a14f60da9d2ca
lvtu <37565148+tongtongyin@users.noreply.github.com>
2020-12-05 04:05:02 +0800

    cherry pick #630 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- pkg/utils/key_test.go
+++ pkg/utils/key_test.go
@@ -13,41 +13,103 @@ type testKeySuite struct{}
 var _ = Suite(&testKeySuite{})
 
 func (r *testKeySuite) TestParseKey(c *C) {
-	rawKey := "1234"
-	parsedKey, err := ParseKey("raw", rawKey)
-	c.Assert(err, IsNil)
-	c.Assert(parsedKey, BytesEquals, []byte(rawKey))
+	// test rawKey
+	testRawKey := []struct {
+		rawKey string
+		ans    []byte
+	}{
+		{"1234", []byte("1234")},
+		{"abcd", []byte("abcd")},
+		{"1a2b", []byte("1a2b")},
+		{"AA", []byte("AA")},
+		{"\a", []byte("\a")},
+		{"\\'", []byte("\\'")},
+	}
 
-	escapedKey := "\\a\\x1"
-	parsedKey, err = ParseKey("escaped", escapedKey)
-	c.Assert(err, IsNil)
-	c.Assert(parsedKey, BytesEquals, []byte("\a\x01"))
+	for _, tt := range testRawKey {
+		parsedKey, err := ParseKey("raw", tt.rawKey)
+		c.Assert(err, IsNil)
+		c.Assert(parsedKey, BytesEquals, tt.ans)
+	}
 
-	hexKey := hex.EncodeToString([]byte("1234"))
-	parsedKey, err = ParseKey("hex", hexKey)
-	c.Assert(err, IsNil)
-	c.Assert(parsedKey, BytesEquals, []byte("1234"))
+	// test EscapedKey
+	testEscapedKey := []struct {
+		EscapedKey string
+		ans        []byte
+	}{
+		{"\\a\\x1", []byte("\a\x01")},
+		{"\\b\\f", []byte("\b\f")},
+		{"\\n\\r", []byte("\n\r")},
+		{"\\t\\v", []byte("\t\v")},
+		{"\\'", []byte("'")},
+	}
 
-	_, err = ParseKey("notSupport", rawKey)
-	c.Assert(err, ErrorMatches, "unknown format.*")
+	for _, tt := range testEscapedKey {
+		parsedKey, err := ParseKey("escaped", tt.EscapedKey)
+		c.Assert(err, IsNil)
+		c.Assert(parsedKey, BytesEquals, tt.ans)
+	}
+
+	// test hexKey
+	testHexKey := []struct {
+		hexKey string
+		ans    []byte
+	}{
+		{"1234", []byte("1234")},
+		{"abcd", []byte("abcd")},
+		{"1a2b", []byte("1a2b")},
+		{"AA", []byte("AA")},
+		{"\a", []byte("\a")},
+		{"\\'", []byte("\\'")},
+		{"\x01", []byte("\x01")},
+		{"\xAA", []byte("\xAA")},
+	}
+
+	for _, tt := range testHexKey {
+		key := hex.EncodeToString([]byte(tt.hexKey))
+		parsedKey, err := ParseKey("hex", key)
+		c.Assert(err, IsNil)
+		c.Assert(parsedKey, BytesEquals, tt.ans)
+	}
+
+	// test other
+	testNotSupportKey := []struct {
+		any string
+		ans []byte
+	}{
+		{"1234", []byte("1234")},
+		{"abcd", []byte("abcd")},
+		{"1a2b", []byte("1a2b")},
+		{"AA", []byte("AA")},
+		{"\a", []byte("\a")},
+		{"\\'", []byte("\\'")},
+		{"\x01", []byte("\x01")},
+		{"\xAA", []byte("\xAA")},
+	}
+
+	for _, tt := range testNotSupportKey {
+		_, err := ParseKey("notSupport", tt.any)
+		c.Assert(err, ErrorMatches, "unknown format.*")
+	}
 }
 
 func (r *testKeySuite) TestCompareEndKey(c *C) {
-	res := CompareEndKey([]byte("1"), []byte("2"))
-	c.Assert(res, Less, 0)
+	// test endKey
+	testCase := []struct {
+		key1 []byte
+		key2 []byte
+		ans  int
+	}{
+		{[]byte("1"), []byte("2"), -1},
+		{[]byte("1"), []byte("1"), 0},
+		{[]byte("2"), []byte("1"), 1},
+		{[]byte("1"), []byte(""), -1},
+		{[]byte(""), []byte(""), 0},
+		{[]byte(""), []byte("1"), 1},
+	}
 
-	res = CompareEndKey([]byte("1"), []byte("1"))
-	c.Assert(res, Equals, 0)
-
-	res = CompareEndKey([]byte("2"), []byte("1"))
-	c.Assert(res, Greater, 0)
-
-	res = CompareEndKey([]byte("1"), []byte(""))
-	c.Assert(res, Less, 0)
-
-	res = CompareEndKey([]byte(""), []byte(""))
-	c.Assert(res, Equals, 0)
-
-	res = CompareEndKey([]byte(""), []byte("1"))
-	c.Assert(res, Greater, 0)
+	for _, tt := range testCase {
+		res := CompareEndKey(tt.key1, tt.key2)
+		c.Assert(res, Equals, tt.ans)
+	}
 }
