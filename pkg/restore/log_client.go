f6bfa6505936148cd759d5bad92990e448839284
luancheng <luancheng@pingcap.com>
2020-12-24 15:03:14 +0800

    fix build

--- pkg/restore/log_client.go
+++ pkg/restore/log_client.go
@@ -28,7 +28,6 @@ import (
 	"github.com/pingcap/br/pkg/cdclog"
 	berrors "github.com/pingcap/br/pkg/errors"
 	"github.com/pingcap/br/pkg/kv"
-	"github.com/pingcap/br/pkg/logutil"
 	"github.com/pingcap/br/pkg/storage"
 	"github.com/pingcap/br/pkg/utils"
 )
