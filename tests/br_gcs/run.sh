86a853d63e0d75e70d79b49e9d9452e7b7f2b366
luancheng <luancheng@pingcap.com>
2021-01-11 19:25:29 +0800

    fix ci

--- tests/br_gcs/run.sh
+++ tests/br_gcs/run.sh
@@ -36,12 +36,12 @@ while ! curl -o /dev/null -v -s "http://$GCS_HOST:$GCS_PORT/"; do
 done
 
 # start oauth server
+killall flask || true
 FLASK_APP=tests/$TEST_NAME/oauth.py flask run &
-OAUTH_ID=$!
 
 stop_gcs() {
     kill -2 $GCS_ID
-    kill -2 $OAUTH_ID
+    killall flask || true
 }
 trap stop_gcs EXIT
 
