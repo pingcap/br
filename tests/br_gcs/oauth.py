00bf666c05f82fe504955f048853222f4bebfc2b
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-01-18 13:22:52 +0800

    test: remove python code  (#699) (#700)
    * cherry pick #699 to release-4.0
    
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    * fix conflict
    
    Co-authored-by: 3pointer <luancheng@pingcap.com>

--- tests/br_gcs/oauth.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/oauth/token', methods=['GET', 'POST'])
-def oauth():
-    return '{"access_token": "ok", "token_type":"service_account", "expires_in":3600}'
