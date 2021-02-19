25b161e205aee159ab3d9a466a9b5e704cf3fbae
yuqi1129 <yuqi4733@gmail.com>
2021-02-19 17:25:07 +0800

    Add log to make error more clear when restore error using s3 sink, fix comment 3

--- pkg/storage/s3.go
+++ pkg/storage/s3.go
@@ -338,8 +338,7 @@ func (rs *S3Storage) ReadFile(ctx context.Context, file string) ([]byte, error) 
 
 	result, err := rs.svc.GetObjectWithContext(ctx, input)
 	if err != nil {
-		er := errors.Trace(err)
-		return nil, errors.Annotatef(er,
+		return nil, errors.Annotatef(err,
 			"failed to read s3 file, file info: input.bucket='%s', input.key='%s'",
 			input.Bucket, input.Key)
 	}
