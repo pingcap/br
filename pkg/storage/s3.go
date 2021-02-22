488e83ff0d3f34cb4a399f2dd4328cb9570d4cc9
yuqi1129 <yuqi4733@gmail.com>
2021-02-22 11:46:36 +0800

    revert some discussion and add log

--- pkg/storage/s3.go
+++ pkg/storage/s3.go
@@ -338,9 +338,8 @@ func (rs *S3Storage) ReadFile(ctx context.Context, file string) ([]byte, error) 
 
 	result, err := rs.svc.GetObjectWithContext(ctx, input)
 	if err != nil {
-		return nil, errors.Annotatef(err,
-			"failed to read s3 file, file info: input.bucket='%s', input.key='%s'",
-			*input.Bucket, *input.Key)
+		log.Error("In read s3 file", zap.Any("file info", input))
+		return nil, errors.Trace(err)
 	}
 	defer result.Body.Close()
 	data, err := ioutil.ReadAll(result.Body)
