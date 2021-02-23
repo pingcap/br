93ae63fc80a94c1e363518aca4dbbd3354c52eb8
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2021-02-23 19:03:20 +0800

    refine br_backup_version test

--- tests/br_backup_version/run.sh
+++ tests/br_backup_version/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright 2021 PingCAP, Inc.
 #
@@ -16,6 +16,24 @@
 set -eu
 DB="$TEST_NAME"
 
+# example
+#    "cluster_id": 6931331682760961243
+expected_cluster_id=`run_curl "https://$PD_ADDR/pd/api/v1/members" | grep "cluster_id"`
+# example
+#"4.0.10"
+expected_cluster_version=`run_curl "https://$PD_ADDR/pd/api/v1/config/cluster-version"`
+
+function check_version() {
+    folder=$1
+    expected_br_version=$2
+    br_version=`run_br -s "local://$TEST_DIR/$folder" debug decode --field "BrVersion"`
+    [[ $br_version =~ $expected_br_version ]]
+    cluster_version=`run_br -s "local://$TEST_DIR/$folder" debug decode --field "ClusterVersion"`
+    [ $cluster_version = $expected_cluster_version ]
+    cluster_id=`run_br -s "local://$TEST_DIR/$folder" debug decode --field "ClusterId"`
+    [[ $expected_cluster_id =~ $cluster_id ]]
+}
+
 # backup empty using BR
 echo "backup start..."
 run_br --pd $PD_ADDR backup full -s "local://$TEST_DIR/br_version_1" --ratelimit 5 --concurrency 4
@@ -24,15 +42,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-run_br -s "local://$TEST_DIR/br_version_1" debug decode --field "BrVersion"
-run_br -s "local://$TEST_DIR/br_version_1" debug decode --field "ClusterVersion"
+check_version "br_version_1" "BR"
 
 # backup empty using BR via SQL
 echo "backup start..."
 run_sql "BACKUP DATABASE $DB TO \"local://$TEST_DIR/br_version_2\""
 
-run_br -s "local://$TEST_DIR/br_version_2" debug decode --field "BrVersion"
-run_br -s "local://$TEST_DIR/br_version_2" debug decode --field "ClusterVersion"
+check_version "br_version_2" "TiDB"
 
 # create a database and insert some data
 run_sql "CREATE DATABASE $DB;"
@@ -52,15 +68,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-run_br -s "local://$TEST_DIR/br_version_3" debug decode --field "BrVersion"
-run_br -s "local://$TEST_DIR/br_version_3" debug decode --field "ClusterVersion"
+check_version "br_version_3" "BR"
 
 # backup tables using BR via SQL
 echo "backup start..."
 run_sql "BACKUP DATABASE $DB TO \"local://$TEST_DIR/br_version_4\""
 
-run_br -s "local://$TEST_DIR/br_version_4" debug decode --field "BrVersion"
-run_br -s "local://$TEST_DIR/br_version_4" debug decode --field "ClusterVersion"
+check_version "br_version_4" "TiDB"
 
 run_sql "DROP DATABASE $DB"
 echo "TEST: [$TEST_NAME] successed!"
