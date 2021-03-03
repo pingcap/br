5fdbb50d1681832fe642336d7ac92a1377aaab1d
glorv <glorvs@163.com>
2021-03-03 15:08:18 +0800

    add integration test

---
+++ tests/lightning_incremental/data/incr.uk_auto_inc-schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `uk_auto_inc` (
+  `u` int PRIMARY KEY,
+  `id` bigint UNIQUE KEY AUTO_INCREMENT,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
---
+++ tests/lightning_incremental/data1/incr.uk_auto_inc.sql
@@ -0,0 +1,5 @@
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `pk_auto_inc` (`id`) VALUES
+(4),
+(5),
+(6);
