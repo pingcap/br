4c9dd6f3640d66cb44b2348a07de688532b1f299
山岚 <36239017+YuJuncen@users.noreply.github.com>
2021-03-08 13:29:23 +0800

    Update pkg/backup/client.go

--- pkg/backup/client.go
+++ pkg/backup/client.go
@@ -780,7 +780,7 @@ func OnBackupResponse(
 	default:
 		// UNSAFE! TODO: use meaningful error code instead of unstructured message to find failed to write error.
 		if messageIsRetryableFailedToWrite(resp.GetError().GetMsg()) {
-			log.Warn("backup occur failed to write file error.", zap.String("error", resp.GetError().GetMsg()))
+			log.Warn("backup occur s3 storage error.", zap.String("error", resp.GetError().GetMsg()))
 			return nil, 3000, nil
 		}
 		log.Error("backup occur unknown error", zap.String("error", resp.Error.GetMsg()), zap.Uint64("storeID", storeID))
