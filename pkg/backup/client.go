445c81fd3b1f3e25979422067e6414b5c1ae4739
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-03-10 19:18:14 +0800

    backupmeta: add cluster_version and br_version info in backupmeta (#734) (#803)
    * cherry pick #734 to release-4.0
    
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    * update kvproto tidb dependencies
    
    Co-authored-by: Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>

--- pkg/backup/client.go
+++ pkg/backup/client.go
@@ -181,12 +181,17 @@ func BuildBackupMeta(
 	files []*kvproto.File,
 	rawRanges []*kvproto.RawRange,
 	ddlJobs []*model.Job,
+	clusterVersion string,
+	brVersion string,
 ) (backupMeta kvproto.BackupMeta, err error) {
 	backupMeta.StartVersion = req.StartVersion
 	backupMeta.EndVersion = req.EndVersion
 	backupMeta.IsRawKv = req.IsRawKv
 	backupMeta.RawRanges = rawRanges
 	backupMeta.Files = files
+	backupMeta.ClusterId = req.ClusterId
+	backupMeta.ClusterVersion = clusterVersion
+	backupMeta.BrVersion = brVersion
 	backupMeta.Ddls, err = json.Marshal(ddlJobs)
 	if err != nil {
 		err = errors.Trace(err)
@@ -205,6 +210,11 @@ func (bc *Client) SaveBackupMeta(ctx context.Context, backupMeta *kvproto.Backup
 	backendURL := storage.FormatBackendURL(bc.backend)
 	log.Info("save backup meta", zap.Stringer("path", &backendURL), zap.Int("size", len(backupMetaData)))
 	return bc.storage.Write(ctx, utils.MetaFile, backupMetaData)
+}
+
+// GetClusterID returns the cluster ID of the tidb cluster to backup.
+func (bc *Client) GetClusterID() uint64 {
+	return bc.clusterID
 }
 
 // BuildTableRanges returns the key ranges encompassing the entire table,
@@ -513,7 +523,6 @@ func (bc *Client) BackupRange(
 		return nil, errors.Trace(err)
 	}
 
-	req.ClusterId = bc.clusterID
 	req.StartKey = startKey
 	req.EndKey = endKey
 	req.StorageBackend = bc.backend
--- pkg/task/backup.go
+++ pkg/task/backup.go
@@ -252,12 +252,18 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 	}
 
 	req := kvproto.BackupRequest{
+		ClusterId:        client.GetClusterID(),
 		StartVersion:     cfg.LastBackupTS,
 		EndVersion:       backupTS,
 		RateLimit:        cfg.RateLimit,
 		Concurrency:      defaultBackupConcurrency,
 		CompressionType:  cfg.CompressionType,
 		CompressionLevel: cfg.CompressionLevel,
+	}
+	brVersion := g.GetVersion()
+	clusterVersion, err := mgr.GetClusterVersion(ctx)
+	if err != nil {
+		return errors.Trace(err)
 	}
 
 	ranges, backupSchemas, err := backup.BuildBackupRangeAndSchema(
@@ -267,7 +273,7 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 	}
 	// nothing to backup
 	if ranges == nil {
-		backupMeta, err2 := backup.BuildBackupMeta(&req, nil, nil, nil)
+		backupMeta, err2 := backup.BuildBackupMeta(&req, nil, nil, nil, clusterVersion, brVersion)
 		if err2 != nil {
 			return errors.Trace(err2)
 		}
@@ -319,7 +325,7 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 	// Backup has finished
 	updateCh.Close()
 
-	backupMeta, err := backup.BuildBackupMeta(&req, files, nil, ddlJobs)
+	backupMeta, err := backup.BuildBackupMeta(&req, files, nil, ddlJobs, clusterVersion, brVersion)
 	if err != nil {
 		return errors.Trace(err)
 	}
