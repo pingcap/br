2c82efc277110219ee4b9efb82c7f86a933aecf2
july2993 <july2993@gmail.com>
2021-04-09 11:14:32 +0800

    Fix check empty table in checkpoint enabled case (#985)

--- pkg/lightning/checkpoints/checkpoints.go
+++ pkg/lightning/checkpoints/checkpoints.go
@@ -663,7 +663,7 @@ func (cpdb *MySQLCheckpointsDB) Initialize(ctx context.Context, cfg *config.Conf
 		for _, db := range dbInfo {
 			for _, table := range db.Tables {
 				tableName := common.UniqueTable(db.Name, table.Name)
-				_, err = stmt.ExecContext(c, cfg.TaskID, tableName, 0, table.ID)
+				_, err = stmt.ExecContext(c, cfg.TaskID, tableName, CheckpointStatusLoaded, table.ID)
 				if err != nil {
 					return errors.Trace(err)
 				}
@@ -781,6 +781,9 @@ func (cpdb *MySQLCheckpointsDB) Get(ctx context.Context, tableName string) (*Tab
 
 		var status uint8
 		if err := tableRow.Scan(&status, &cp.AllocBase, &cp.TableID); err != nil {
+			if err == sql.ErrNoRows {
+				return errors.NotFoundf("checkpoint for table %s", tableName)
+			}
 			return errors.Trace(err)
 		}
 		cp.Status = CheckpointStatus(status)
@@ -1043,7 +1046,7 @@ func (cpdb *FileCheckpointsDB) Get(_ context.Context, tableName string) (*TableC
 
 	tableModel, ok := cpdb.checkpoints.Checkpoints[tableName]
 	if !ok {
-		tableModel = &checkpointspb.TableCheckpointModel{}
+		return nil, errors.NotFoundf("checkpoint for table %s", tableName)
 	}
 
 	cp := &TableCheckpoint{
