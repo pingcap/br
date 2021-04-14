fa79db2cbe10f07c54a74bce90dd5487ae094ce6
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2021-04-14 11:06:03 +0800

    address comment

--- pkg/logutil/logging.go
+++ pkg/logutil/logging.go
@@ -187,8 +187,8 @@ func ShortError(err error) zap.Field {
 	return zap.String("error", err.Error())
 }
 
-// Any constructs a field that carries an interface{}.
-func Any(fieldKey string, key interface{}) zap.Field {
+// RedactAny constructs a redacted field that carries an interface{}.
+func RedactAny(fieldKey string, key interface{}) zap.Field {
 	if redact.NeedRedact() {
 		return zap.String(fieldKey, "?")
 	}
