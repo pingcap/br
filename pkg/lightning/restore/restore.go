8760a4f47be6ed39caf0df0821f1a2cdaeedbc24
3pointer <luancheng@pingcap.com>
2021-05-20 11:57:44 +0800

    Update restore.go

--- pkg/lightning/restore/restore.go
+++ pkg/lightning/restore/restore.go
@@ -2595,9 +2595,7 @@ func (cr *chunkRestore) encodeLoop(
 				canDeliver = true
 				kvSize = 0
 			}
-				canDeliver = true
-				kvSize = 0
-			}
+
 			if len(kvPacket) >= maxKvPairsCnt || newOffset == cr.chunk.Chunk.EndOffset {
 				canDeliver = true
 			}
