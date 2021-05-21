858049deefd577cae8f072f478e8d42cb02962a1
zhangwenjum <2780898229@qq.com>
2021-05-21 12:42:24 +0800

    resolve conflict

--- pkg/task/restore.go
+++ pkg/task/restore.go
@@ -236,12 +236,6 @@ func RunRestore(c context.Context, g glue.Glue, cmdName string, cfg *RestoreConf
 	}
 
 	g.Record(summary.RestoreDataSize, utils.ArchiveSize(backupMeta))
-	backupVersion := version.NormalizeBackupVersion(backupMeta.ClusterVersion)
-	if cfg.CheckRequirements && backupVersion != nil {
-		if versionErr := version.CheckClusterVersion(ctx, mgr.GetPDClient(), version.CheckVersionForBackup(backupVersion)); versionErr != nil {
-			return errors.Trace(versionErr)
-		}
-	}
 
 	if err = client.InitBackupMeta(backupMeta, u); err != nil {
 		return errors.Trace(err)
