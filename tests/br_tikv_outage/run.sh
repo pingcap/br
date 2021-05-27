551adf0e5a34b19ae3c965fb14bef534a689e280
Leavrth <Legenull@gmail.com>
2021-05-27 15:24:52 +0800

    delete the former commit
    Signed-off-by: Leavrth <Legenull@gmail.com>

--- tests/br_tikv_outage/run.sh
+++ tests/br_tikv_outage/run.sh
@@ -54,8 +54,7 @@ hint_backup_start=$TEST_DIR/hint_backup_start
 hint_get_backup_client=$TEST_DIR/hint_get_backup_client
 
 
-# cases=${cases:-'outage outage-after-request outage-at-finegrained shutdown scale-out'}
-cases=${cases:-'outage outage-after-request shutdown scale-out'}
+cases=${cases:-'outage outage-after-request outage-at-finegrained shutdown scale-out'}
 
 for failure in $cases; do
     rm -f "$hint_finegrained" "$hint_backup_start" "$hint_get_backup_client"
