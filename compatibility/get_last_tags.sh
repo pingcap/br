8cb9a3f43bc9031cf655d76d091b361b2dd2aedf
Leavrth <Legenull@gmail.com>
2021-06-01 18:07:46 +0800

    make it complete
    Signed-off-by: Leavrth <Legenull@gmail.com>

--- compatibility/get_last_tags.sh
+++ compatibility/get_last_tags.sh
@@ -5,15 +5,16 @@ getLatestTags() {
   release_5_branch_regex="^release-5\.[0-9].*$"
   release_4_branch_regex="^release-4\.[0-9].*$"
   TOTAL_TAGS=$(git for-each-ref --sort=creatordate  refs/tags | awk -F '/' '{print $3}')
+  filter='alpha'
   # latest tags
-  TAGS=$(echo $TOTAL_TAGS | tr ' ' '\n' | grep -v 'alpha' | tail -n3)
+  TAGS=$(echo $TOTAL_TAGS | tr ' ' '\n' | grep -v $filter | tail -n3)
   if git rev-parse --abbrev-ref HEAD | egrep -q $release_5_branch_regex
   then
     # If we are in release-5.0 branch, try to use latest 3 version of 5.x and last 4.x version
-    TAGS=$(echo $TOTAL_TAGS | tr ' ' '\n' | fgrep "v4." | tail -n1 && echo $TOTAL_TAGS | tr ' ' '\n' | fgrep "v5." | tail -n3)
+    TAGS=$(echo $TOTAL_TAGS | tr ' ' '\n' | fgrep "v4." | grep -v $filter | tail -n1 && echo $TOTAL_TAGS | tr ' ' '\n' | fgrep "v5." | grep -v $filter | tail -n3)
   elif git rev-parse --abbrev-ref HEAD | egrep -q $release_4_branch_regex
   then
     # If we are in release-4.0 branch, try to use latest 3 version of 4.x
-    TAGS=$(echo $TOTAL_TAGS | tr ' ' '\n' | fgrep "v4." | tail -n3)
+    TAGS=$(echo $TOTAL_TAGS | tr ' ' '\n' | fgrep "v4." | grep -v $filter | tail -n3)
   fi
 }
