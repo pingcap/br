0c47e512465da15289914cf58763938eb853a74f
Leavrth <Legenull@gmail.com>
2021-06-02 10:22:27 +0800

    add ensure_tikv into kv_outage
    Signed-off-by: Leavrth <Legenull@gmail.com>

--- tests/br_tikv_outage/run.sh
+++ tests/br_tikv_outage/run.sh
@@ -71,8 +71,6 @@ github.com/pingcap/br/pkg/conn/hint-get-backup-client=1*return(\"$hint_get_backu
     backup_pid=$!
     single_point_fault $failure
     wait $backup_pid
-    # let tikv start up completely if backup is finished before tikv restarts
-    ensure_tikv
     case $failure in
     scale-out | shutdown | outage-at-finegrained ) stop_services
         start_services ;;
