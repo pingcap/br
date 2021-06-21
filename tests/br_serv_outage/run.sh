ffe8488a48a857339734baa22d9eaf87c34ab6e7
Leavrth <Legenull@gmail.com>
2021-06-21 11:45:16 +0800

    add annotation

--- tests/br_serv_outage/run.sh
+++ tests/br_serv_outage/run.sh
@@ -53,7 +53,8 @@ hint_finegrained=$TEST_DIR/hint_finegrained
 hint_backup_start=$TEST_DIR/hint_backup_start
 hint_get_backup_client=$TEST_DIR/hint_get_backup_client
 
-
+# NOTE : cases `outage-at-finegrained shutdown` should be former to avoid issue
+#       https://github.com/pingcap/br/issues/1050
 cases=${cases:-'outage-at-finegrained shutdown scale-out outage outage-after-request'}
 
 for failure in $cases; do
