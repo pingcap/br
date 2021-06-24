650c7d8cd1b9a66b8d3e757baa838e1b006885af
Yujie Xia <xiayjchn@gmail.com>
2021-06-24 16:50:57 +0800

    fix integration test

--- tests/lightning_cmdline_override/run.sh
+++ tests/lightning_cmdline_override/run.sh
@@ -11,6 +11,7 @@ run_lightning \
     --tidb-status 10080 \
     --pd-urls 127.0.0.1:2379 \
     -d "tests/$TEST_NAME/data" \
+    --backend 'importer' \
     --importer 127.0.0.1:8808
 
 run_sql 'SELECT * FROM cmdline_override.t'
