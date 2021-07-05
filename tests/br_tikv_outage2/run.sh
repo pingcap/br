3281b9bb8d8fa48593c7d5a0f58888ddf1ee96c3
liaojj <36503113+Leavrth@users.noreply.github.com>
2021-07-05 09:36:03 +0800

    Update run.sh

--- tests/br_tikv_outage2/run.sh
+++ tests/br_tikv_outage2/run.sh
@@ -12,8 +12,6 @@ hint_finegrained=$TEST_DIR/hint_finegrained
 hint_backup_start=$TEST_DIR/hint_backup_start
 hint_get_backup_client=$TEST_DIR/hint_get_backup_client
 
-# NOTE : cases `outage-at-finegrained shutdown scale-out` should be first to avoid issue
-#       https://github.com/pingcap/br/issues/1050
 cases=${cases:-'outage-at-finegrained outage outage-after-request'}
 
 for failure in $cases; do
