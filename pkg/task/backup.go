b217b4f46b9b346c29c66ece62a3d85f8b7c535d
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2021-07-13 11:24:59 +0800

    try to fix

--- pkg/task/backup.go
+++ pkg/task/backup.go
@@ -333,7 +333,6 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 	var (
 		updateCh glue.Progress
 		unit     backup.ProgressUnit
-		f        *os.File
 	)
 	if len(ranges) < 100 {
 		unit = backup.RegionUnit
@@ -366,14 +365,14 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 			failpoint.Inject("progress-call-back", func(v failpoint.Value) {
 				log.Info("failpoint progress-call-back injected")
 				if fileName, ok := v.(string); ok {
-					f, err = os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, os.ModePerm)
-					if err != nil {
-						log.Warn("failed to create file", zap.Error(err))
+					f, err2 := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+					if err2 != nil {
+						log.Warn("failed to create file", zap.Error(err2))
 					}
 					msg := []byte(fmt.Sprintf("%s:%d\n", unit, progressCount))
-					_, err = f.Write(msg)
-					if err != nil {
-						log.Warn("failed to write data to file", zap.Error(err))
+					_, err2 = f.Write(msg)
+					if err2 != nil {
+						log.Warn("failed to write data to file", zap.Error(err2))
 					}
 				}
 			})
