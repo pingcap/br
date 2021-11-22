7a48d74c5d882440e6b8e86f822489798e1ef8fe
glorv <glorvs@163.com>
2021-11-22 15:43:48 +0800

    lightning: fix infinite loop in merge ssts (#1430)

--- pkg/lightning/backend/local/local.go
+++ pkg/lightning/backend/local/local.go
@@ -2976,7 +2976,9 @@ func (i dbSSTIngester) mergeSSTs(metas []*sstMeta, dir string) (*sstMeta, error)
 	for {
 		if bytes.Equal(lastKey, key) {
 			log.L().Warn("duplicated key found, skipped", zap.Binary("key", lastKey))
-			continue
+			newMeta.totalCount--
+			newMeta.totalSize -= int64(len(key) + len(val))
+			goto nextKey
 		}
 		internalKey.UserKey = key
 		err = writer.Add(internalKey, val)
@@ -2984,6 +2986,7 @@ func (i dbSSTIngester) mergeSSTs(metas []*sstMeta, dir string) (*sstMeta, error)
 			return nil, err
 		}
 		lastKey = append(lastKey[:0], key...)
+	nextKey:
 		key, val, err = mergeIter.Next()
 		if err != nil {
 			return nil, err
